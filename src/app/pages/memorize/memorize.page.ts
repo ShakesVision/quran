ecf925fb6ba87fcefeef99f06237073b3cbd4aa5
Shakeeb Ahmad <shakeeb.ahmad@simplifiedautomation.ca>
2022-03-12 23:35:51 +0530

    icons, recom memorizations, surah.json

--- src/app/pages/memorize/memorize.page.ts
+++ src/app/pages/memorize/memorize.page.ts
@@ -1,20 +1,93 @@
-import { Component, OnInit } from '@angular/core';
-import { Storage } from '@ionic/storage-angular';
+import { Component, OnInit } from "@angular/core";
+import { AlertController } from "@ionic/angular";
+import { Storage } from "@ionic/storage-angular";
+import { Observable, Subject } from "rxjs";
 
 @Component({
-  selector: 'app-memorize',
-  templateUrl: './memorize.page.html',
-  styleUrls: ['./memorize.page.scss'],
+  selector: "app-memorize",
+  templateUrl: "./memorize.page.html",
+  styleUrls: ["./memorize.page.scss"],
 })
 export class MemorizePage implements OnInit {
+  items: Array<Object> = [];
+  recommendedChapters = [
+    'الفاتحہ',
+    'یس',
+    'رحمن',
+    'واقعہ',
+    'ملک',
+    'کہف '  
+  ]
+  constructor(
+    private storage: Storage,
+    private alertController: AlertController
+  ) {}
 
-  constructor(private storage: Storage) { }
-
-  async ngOnInit() {    
+  async ngOnInit() {
     await this.storage.create();
-    this.storage.set("v1",{mother:"son"}).then(res=>console.log(res));
-    this.storage.set("v2",{mother:"son"}).then(res=>console.log(res));
-    localStorage.setItem("v1","son");
+    this.storage.set("v1", { mother: "son" }).then((res) => console.log(res));
+    this.storage.set("v2", { mother: "son" }).then((res) => console.log(res));
+    localStorage.setItem("v1", "son");
   }
 
+  async add() {
+    const alert = await this.alertController.create({
+      subHeader: "Add",
+      inputs: [
+        {
+          name: "date",
+          id: "date",
+          type: "date",
+          placeholder: "Date...",
+          value: new Date()
+        },
+        {
+          name: "surah",
+          id: "surah",
+          type: "search",
+          placeholder: "Surah name...",
+          handler: (input)=>{
+            console.log('Inside handler:',input);
+          }
+        },
+        {
+          name: 'from',
+          id: 'from',
+          type: 'number',
+          placeholder: "From Ayah number..."
+        },
+        {
+          name: 'to',
+          id: 'to',
+          type: 'number',
+          placeholder: "To Ayah number..."
+        },
+        {
+          name: 'finished',
+          id: 'finished',
+          type: 'checkbox',
+          label: 'Finished',
+          handler: ()=>{
+            console.log('checkbox selected.');
+          },
+          checked: true
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Add',
+          handler: (data)=> {
+            console.log(data);
+            this.items.push(data);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+  getFormattedDate = (date) => new Date(date).toLocaleDateString();
 }

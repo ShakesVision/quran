30f1a4fd3d6a6458297c4de1278cf8e352e63240
Shakeeb Ahmad <shakeeb.ahmad@simplifiedautomation.ca>
2022-03-13 16:13:43 +0530

    localhost runs on http, replace with https in prod

--- src/app/pages/listen/listen.page.ts
+++ src/app/pages/listen/listen.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-listen',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listen.page.scss'],
 })
 export class ListenPage implements OnInit {
+  ayahNumber:number;
+  audioSrc:string;
+  media: HTMLAudioElement;
 
+  @ViewChild('audioEl') audioEl:ElementRef<HTMLAudioElement>;
   constructor() { }
 
   ngOnInit() {
+    this.media = <HTMLAudioElement>document.getElementById('audio');
+
+  }
+
+  get $player(): HTMLAudioElement {
+    return this.audioEl.nativeElement;
+  } 
+
+  listen(ayahNumber) {
+    this.media.pause();
+    // replace http with https in prod
+    this.audioSrc = `http://cdn.islamic.network/quran/audio/64/ar.abdurrahmaansudais/${ayahNumber}.mp3`;
+    console.log(this.audioEl);
+    this.media.src = this.audioSrc;
+    this.media.play();
   }
 
 }

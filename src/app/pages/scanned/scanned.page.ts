4cd01aa351ade7038c77f536fd3bc0f9baa2a80a
Shakeeb Ahmad <shakeeb.ahmad@simplifiedautomation.ca>
2022-04-20 08:54:27 +0530

    surah number jump feature

--- src/app/pages/scanned/scanned.page.ts
+++ src/app/pages/scanned/scanned.page.ts
@@ -13,6 +13,17 @@ export class ScannedPage implements OnInit {
   identifier: string;
   imgQuality: ImageQuality = ImageQuality.High;
   ImageQuality = ImageQuality;
+  surahPageNumbers = [
+    2, 3, 51, 78, 107, 129, 152, 178, 188, 209, 222, 236, 250, 256, 262, 268,
+    283, 294, 306, 313, 323, 332, 343, 351, 360, 367, 377, 386, 397, 405, 412,
+    416, 419, 429, 435, 441, 446, 453, 459, 468, 478, 484, 490, 496, 499, 503,
+    507, 512, 516, 519, 521, 524, 527, 529, 532, 535, 538, 543, 546, 550, 552,
+    554, 555, 557, 559, 561, 563, 565, 568, 570, 572, 574, 577, 579, 581, 583,
+    585, 587, 588, 590, 591, 592, 593, 595, 596, 597, 598, 598, 599, 601, 601,
+    602, 603, 603, 604, 604, 605, 605, 606, 606, 607, 607, 608, 608, 608, 609,
+    609, 609, 609, 610, 610, 610, 611, 611,
+  ];
+
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
@@ -34,6 +45,10 @@ export class ScannedPage implements OnInit {
     console.log(p, quality);
     let paddedPageNumber = String(p).padStart(4, "0");
     this.url = `${this.incompleteUrl}${paddedPageNumber}.jp2&id=${this.identifier}&scale=${quality}&rotate=0`;
+  }
+  jumpToSurah(n: number) {
+    console.log(n);
+    this.loadImg(this.surahPageNumbers[n - 1], this.imgQuality);
   }
 }
 export enum ImageQuality {

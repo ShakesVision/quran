e989280ee04ff61bd56c02e5e1f19732719bac3f
Shakeeb Ahmad <shakes.ahmad@gmail.com>
2022-11-29 01:24:15 +0530

    remotely updating about etc page and modal implementation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { ProgressPage } from "../pages/progress/progress.page";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
-  constructor() {
-      
+  constructor(private modalController: ModalController) {}
+  ngOnInit() {}
+  async open(name) {
+    const modal = await this.modalController.create({
+      component: ProgressPage,
+      componentProps: { name },
+      swipeToClose: true,
+    });
+    modal.present();
   }
-
 }
--- src/app/pages/progress/progress.page.ts
+++ src/app/pages/progress/progress.page.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { ModalController, NavParams } from "@ionic/angular";
+import { ComponentProps } from "@ionic/core";
 
 @Component({
-  selector: 'app-progress',
-  templateUrl: './progress.page.html',
-  styleUrls: ['./progress.page.scss'],
+  selector: "app-progress",
+  templateUrl: "./progress.page.html",
+  styleUrls: ["./progress.page.scss"],
 })
 export class ProgressPage implements OnInit {
-
-  constructor() { }
+  name: string = this.navParams.get("name");
+  url: string;
+  data: string;
+  loading = false;
+  constructor(
+    private httpClient: HttpClient,
+    private navParams: NavParams,
+    private modalController: ModalController
+  ) {}
 
   ngOnInit() {
+    this.loading = true;
+    this.url = `https://cdn.jsdelivr.net/gh/ShakesVision/Quran_archive@master/App/${this.name}.html`;
+    this.httpClient.get(this.url, { responseType: "text" }).subscribe((res) => {
+      this.data = res;
+      this.loading = false;
+    });
   }
-
+  dismissModal() {
+    this.modalController.dismiss();
+  }
 }

d39602fe7250b95d4ac953293378b9e26890910c
Shakeeb Ahmad <shakes.ahmad@gmail.com>
2022-12-01 00:36:15 +0530

    add other pages from the git, add dom sanitizer

--- src/app/pages/progress/progress.page.ts
+++ src/app/pages/progress/progress.page.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 import { ModalController, NavParams } from "@ionic/angular";
 import { ComponentProps } from "@ionic/core";
 
@@ -11,19 +12,20 @@ import { ComponentProps } from "@ionic/core";
 export class ProgressPage implements OnInit {
   name: string = this.navParams.get("name");
   url: string;
-  data: string;
+  data: SafeHtml;
   loading = false;
   constructor(
     private httpClient: HttpClient,
     private navParams: NavParams,
-    private modalController: ModalController
+    private modalController: ModalController,
+    private domSanatizer: DomSanitizer
   ) {}
 
   ngOnInit() {
     this.loading = true;
     this.url = `https://cdn.jsdelivr.net/gh/ShakesVision/Quran_archive@master/App/${this.name}.html`;
     this.httpClient.get(this.url, { responseType: "text" }).subscribe((res) => {
-      this.data = res;
+      this.data = this.domSanatizer.bypassSecurityTrustHtml(res);
       this.loading = false;
     });
   }

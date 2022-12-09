dd88a14b9024fedb1851a78307db34ca519fb2ec
Shakeeb Ahmad <shakes.ahmad@gmail.com>
2022-12-09 09:36:38 +0530

    new:fullscreen config, login-signup alert set up

--- src/app/pages/surah/surah.page.ts
+++ src/app/pages/surah/surah.page.ts
@@ -3,6 +3,7 @@ import { Observable } from "rxjs";
 import { Router, ActivatedRoute } from "@angular/router";
 import { SurahService } from "./../../services/surah.service";
 import { Surah, Index } from "./../../services/surah";
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: "app-surah",
@@ -12,7 +13,11 @@ import { Surah, Index } from "./../../services/surah";
 export class SurahPage implements OnInit {
   items: Observable<Index[]>;
 
-  constructor(private surahService: SurahService, private router: Router) {}
+  constructor(
+    private surahService: SurahService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
   ngOnInit() {
     // this.items = this.surahService.getSurahs();
     this.items = this.surahService.getIndexes();
@@ -25,5 +30,57 @@ export class SurahPage implements OnInit {
       this.surahService.currentSurah = res;
       this.router.navigate(["/read"]);
     });
+  }
+
+  async loginAlert(item?) {
+    console.log(item);
+    const alert = await this.alertController.create({
+      header: "Login / Signup",
+      cssClass: "custom-alert",
+      inputs: [
+        {
+          name: "email",
+          id: "email",
+          type: "email",
+          placeholder: "Email...",
+          value: item ? item.email : null,
+        },
+        {
+          name: "password",
+          id: "password",
+          type: "password",
+          placeholder: "Password...",
+          value: item ? item.password : null,
+        },
+      ],
+      buttons: [
+        {
+          ...(item
+            ? {
+                text: "Logout",
+                handler: (data) => {
+                  console.log("Logout");
+                },
+                cssClass: "delete-btn",
+              }
+            : null),
+        },
+        {
+          text: "Signup",
+          cssClass: "signup-btn",
+          handler: (data) => {
+            console.log(data);
+          },
+        },
+        {
+          text: "Login",
+          cssClass: "add-btn",
+          handler: (data) => {
+            console.log(data);
+          },
+        },
+      ],
+    });
+    alert.present();
   }
 }

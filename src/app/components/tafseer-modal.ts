e89916279727bbe3d5be6c2aa2cc305feaf352da
Shakeeb Ahmad <shakes.ahmad@gmail.com>
2025-02-10 00:10:00 +0530

    Updated words table style, added tafsir ibn kathir (ar,ur) from git.

--- src/app/components/tafseer-modal.ts
+++ src/app/components/tafseer-modal.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, Input } from "@angular/core";
 import { ModalController } from "@ionic/angular";
+import { finalize, map } from "rxjs/operators";
 
 @Component({
   selector: "app-tafseer-modal",
@@ -11,8 +13,16 @@ export class TafseerModalComponent {
   @Input() verse;
   audioSrc: string;
   baseurl = "https://verses.quran.com/";
+  tafsir = {
+    ar: null,
+    ur: null,
+  };
+  loading = false;
 
-  constructor(private modalCtrl: ModalController) {}
+  constructor(
+    private modalCtrl: ModalController,
+    private httpClient: HttpClient
+  ) {}
 
   ngOnInit() {
     this.audioSrc = `${this.baseurl}${this.verse.audio.url}`;
@@ -27,5 +37,20 @@ export class TafseerModalComponent {
     // Using the injected ModalController this component
     // can close itself
     this.modalCtrl.dismiss();
+  }
+  loadTafsir(lang: string) {
+    this.loading = true;
+    const [s, a] = this.verse.verse_key.split(":");
+    const url =
+      lang === "ar"
+        ? `https://cdn.jsdelivr.net/gh/spa5k/tafsir_api@main/tafsir/ar-tafsir-ibn-kathir/${s}/${a}.json`
+        : `https://cdn.jsdelivr.net/gh/spa5k/tafsir_api@main/tafsir/ur-tafseer-ibn-e-kaseer/${s}/${a}.json`;
+    return this.httpClient
+      .get<any>(url)
+      .pipe(
+        map((res) => res.text),
+        finalize(() => (this.loading = false))
+      )
+      .subscribe((res) => (this.tafsir[lang] = res));
   }
 }
